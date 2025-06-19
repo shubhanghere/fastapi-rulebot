4975c1156dbe28a055c72a6efeb8c2b69bc8abb2
Coder_Sam <116829188+shubhanghere@users.noreply.github.com>
2025-06-19 13:40:19 +0530

    Main.py: Updated main.py file with tracking enabled routing

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.config import RULES
+import json
 
 app = FastAPI()
 
@@ -12,6 +13,18 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "message": "", "link": ""})
+
+@app.post("/track")
+async def track_user_activity(request: Request):
+    data = await request.json()  # data holding the values from the F/E tracked values
+    print("Tracking data recieved:", data)
+    
+    
+#Logging to the file 
+    with open("user_logs.json","a") as f:
+        f.write(json.dumps(data)+ "\n") 
+
+    return{"status": "ok"}
 
 @app.get("/about/{topic}", response_class=HTMLResponse)
 async def about_topic(request: Request, topic: str):
